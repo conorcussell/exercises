6983c86e2ddee054db8d3a732db6e73dd6874a3c
conorcussell <conor@thetab.com>
2018-01-19 10:27:23 +0000

    Something

--- binary-search/index.js
+++ binary-search/index.js
@@ -1,55 +1,23 @@
-function binarySearch(arr, query) {
-  var array = arr.sort();
-  var match = query;
+function binarySearch(array, query) {
+  array.sort();
 
-  var newArray;
+  if (!array.length) return -1;
 
-  // if one item in array
-  if (array.length == 1 && array[0] == match) {
-    if(array[0] == match) {
-      console.log(array[0] == match, 'these match!!');
-      return 0;
-    };
+  var min = 0;
+  var max = array.length - 1;
+  var guess;
+
+  while (max < min) {
+    guess = Math.floor(max / min);
+    if (array[guess] === query) {
+      return guess;
+    } else if (array[guess] < query) {
+      min = guess + 1;
+    } else {
+      max = guess - 1;
+    }
   }
-
-  if (array.length == 1 && array[0] != match) {
-    console.log('there is one item, and it doesnt match');
-    return -1;
-  }
-
-  // covers empty array
-  if (array.length == 0) {
-    console.log('the array is empty');
-    return -1;
-  }
-
-  // find the middle
-
-  var mid = Math.floor((array.length - 1) / 2);
-
-  // check if middle matches query return if so
-
-  if (array[mid] === match) {
-    return mid;
-  }
-
-  // other wise chop up the array and call binarySearch again
-
-  if (array[mid] > match) {
-    var secondidx = mid - 1;
-    newArray = array.slice(0, secondidx);
-    console.log('get first half', newArray, match);
-    binarySearch(newArray, match);
-  } else {
-    var secondidx = mid + 1;
-    newArray = array.slice(secondidx);
-    console.log('get second half', newArray, match);
-    binarySearch(newArray, match);
-  }
-
-  // why does this always get returned?
   return -1;
-
-};
+}
 
 module.exports = binarySearch;
