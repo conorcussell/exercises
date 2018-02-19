de0884e205291ad1e2716da8aac052a030bd6983
Conor Cussell <contact@conorcussell.co.uk>
2018-02-19 19:03:03 +0000

    more readable solution

--- invert-tree/test.js
+++ invert-tree/test.js
@@ -3,13 +3,13 @@ var invertTree = require('./');
 
 describe('invert-tree', function() {
   it('inverts a binary tree', function() {
-    var root = {value: 6};
-    var left = {value: 4};
-    var right = {value: 8};
-    var leftOfLeft = {value: 3};
-    var rightOfLeft = {value: 5};
-    var leftOfRight = {value: 7};
-    var rightOfRight = {value: 9};
+    var root = { value: 6 };
+    var left = { value: 4 };
+    var right = { value: 8 };
+    var leftOfLeft = { value: 3 };
+    var rightOfLeft = { value: 5 };
+    var leftOfRight = { value: 7 };
+    var rightOfRight = { value: 9 };
     root.left = left;
     root.right = right;
     left.left = leftOfLeft;
@@ -20,32 +20,32 @@ describe('invert-tree', function() {
     invertTree(root);
 
     assert.deepEqual(root, {
-      "value": 6,
-      "left": {
-        "value": 8,
-        "left": {
-          "value": 9
+      value: 6,
+      left: {
+        value: 8,
+        left: {
+          value: 9
         },
-        "right": {
-          "value": 7
+        right: {
+          value: 7
         }
       },
-      "right": {
-        "value": 4,
-        "left": {
-          "value": 5
+      right: {
+        value: 4,
+        left: {
+          value: 5
         },
-        "right": {
-          "value": 3
+        right: {
+          value: 3
         }
       }
     });
   });
 
   it('inverts a right-leaning asymmetric tree', function() {
-    var root = {value: 4};
-    var right = {value: 6};
-    var rightOfRight = {value: 8};
+    var root = { value: 4 };
+    var right = { value: 6 };
+    var rightOfRight = { value: 8 };
     root.right = right;
     right.right = rightOfRight;
 
@@ -54,10 +54,10 @@ describe('invert-tree', function() {
     root = JSON.parse(JSON.stringify(root));
 
     assert.deepEqual(root, {
-      "value": 4,
-      "left": {
-        "value": 6,
-        "left": {
+      value: 4,
+      left: {
+        value: 6,
+        left: {
           value: 8
         }
       }
@@ -65,9 +65,9 @@ describe('invert-tree', function() {
   });
 
   it('inverts a left-leaning asymmetric tree', function() {
-    var root = {value: 5};
-    var left = {value: 7};
-    var leftOfLeft = {value: 9};
+    var root = { value: 5 };
+    var left = { value: 7 };
+    var leftOfLeft = { value: 9 };
     root.left = left;
     left.left = leftOfLeft;
 
@@ -76,10 +76,10 @@ describe('invert-tree', function() {
     root = JSON.parse(JSON.stringify(root));
 
     assert.deepEqual(root, {
-      "value": 5,
-      "right": {
-        "value": 7,
-        "right": {
+      value: 5,
+      right: {
+        value: 7,
+        right: {
           value: 9
         }
       }
